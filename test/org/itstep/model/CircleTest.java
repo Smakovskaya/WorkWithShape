d400ed09c94001483b799bd18f877e3a10390a74
Smakovskaya <Anytochka4575341>
2019-05-30 20:02:05 +0300

    Add tests

---
+++ test/org/itstep/model/CircleTest.java
@@ -0,0 +1,16 @@
+package org.itstep.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @Test
+    void getArea() {
+        Circle circle = new Circle();
+        circle.setRadius(10);
+        double testData = circle.getArea();
+        assertEquals
+    }
+}
